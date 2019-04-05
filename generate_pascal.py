1bf1202ee2e81edecbd53efbe588e5bf7b2bf9ec
Stephen Piatkowski <stevepiatkowski@aol.com>
2019-04-05 16:04:43 +0100

    Change calculation to avoid numerical errors

--- generate_pascal.py
+++ generate_pascal.py
@@ -12,15 +12,23 @@ class Solution():
 
     def calcNode(self,k,l):
         val = 1
+        # need to calculate product first else the division can cause
+        # rounding errors.  By calculating in this order we are also sure
+        # that the numerator can be divided, without remainder, by the 
+        # denominator.  Also, we need to alternate the product and division 
+        # else the product/factorial in the numerator can also lead to 
+        # numerical inaccuracies.
         for i in range(1,l+1):
-            val *= (k+1-i)/(i)
+            val *= (k+1-i)
+            val /= i
+        
         #print("{}C{} is {}".format(k+1,l+1,int(val)))
         return int(val)
 
 def main():
     print("Debug line one")
     y = Solution()
-    print(y.generate(5))
+    print(y.generate(12))
 
 if __name__ == "__main__":
     main()
