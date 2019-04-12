944be91b16fd0af09668c9a86989de3a99983786
Stephen Piatkowski <stevepiatkowski@aol.com>
2019-04-12 23:00:10 +0100

    testing using unittest framework

--- test_palindrome.py
+++ test_palindrome.py
@@ -1,14 +1,22 @@
 # Test file for calling imported library functions
-
+import unittest
 from longestPalindrome import Solution
 
+class TestPalindromeSuite(unittest.TestCase):
 
-def main():
-    y = Solution()
-    x = y.longestPalindrome('jjghefiooifellabbxb')
-    print(x)
-    x = y.longestPalindrome('')
-    print(x)
+    def test_longer(self):
+        y = Solution()
+        test_cases = ["aaaa","aba","abasskjhghjkz","abasskjhgghjkz"]
+        responses = ["aaaa","aba","kjhghjk","kjhgghjk"]
+        for i in range(len(test_cases)):
+            self.assertEqual(y.longestPalindrome(test_cases[i]), responses[i])
 
-if __name__=="__main__":
-    main()     
+    def test_extreme(self):
+        y = Solution()
+        test_cases = ["","a"," "]
+        responses = ["","a"," "]
+        for i in range(len(test_cases)):
+            self.assertEqual(y.longestPalindrome(test_cases[i]), responses[i])
+
+if __name__ == '__main__':
+    unittest.main()
